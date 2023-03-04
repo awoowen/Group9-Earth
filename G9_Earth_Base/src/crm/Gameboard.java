0bb705e0b29a6b4dca10dc5248e54e813e76f9be
JoeyAlphaBBQ <89522198+JoeyAlphaBBQ@users.noreply.github.com>
2023-03-03 20:37:27 -0800

    Update Gameboard.java

--- G9_Earth_Base/src/crm/Gameboard.java
+++ G9_Earth_Base/src/crm/Gameboard.java
@@ -4,21 +4,92 @@ import java.util.ArrayList;
 
 public class Gameboard {
 
-	private int sprouts;
-	private int soilTokens;
-	private int growthTrunk;
-	private int growthCanopy;
+	private int sprout;
+	private int soil;
+	private int trunk;
+	private int canopy;
 	private ArrayList<Card> earthCardDeck;
 	private ArrayList<Card> discardPile;
-	private Faunaboard currFauna;
+	private Player p1, p2, p3, p4, p5;
 	
 	public Gameboard() {
-		// TODO Auto-generated constructor stub
+		setSprout();
+		setSoil();
+		setTrunk();
+		setCanopy();
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Gameboard thisGame = new Gameboard();
+		
 
 	}
 
+	public void setSprout() {
+		sprout = 1000;
+	}
+	
+	public void adjustSprout(int amtSprout) {
+		if(sprout + amtSprout <= 0) {
+			setSprout();
+			sprout += amtSprout;
+		} else {
+			sprout += amtSprout;
+		}
+	}
+	
+	public int getSprout() {
+		return sprout;
+	}
+	
+	public void setSoil() {
+		soil = 1000;
+	}
+	
+	public void adjustSoil(int amtSoil) {
+		if(soil + amtSoil <= 0) {
+			setSoil();
+			soil += amtSoil;
+		} else {
+			soil += amtSoil;
+		}
+	}
+	
+	public int getSoil() {
+		return soil;
+	}
+	
+	public void setTrunk() {
+		trunk = 500;
+	}
+	
+	public void adjustTrunk(int amtTrunk) {
+		if(trunk + amtTrunk <= 0) {
+			setTrunk();
+			trunk += amtTrunk;
+		} else {
+			trunk += amtTrunk;
+		}
+	}
+	
+	public int getTrunk() {
+		return trunk;
+	}
+	
+	public void setCanopy() {
+		canopy = 100;
+	}
+	
+	public void adjustCanopy(int amtCanopy) {
+		if(canopy + amtCanopy <=0) {
+			setCanopy();
+			canopy += amtCanopy;
+		} else {
+			canopy += amtCanopy;
+		}
+	}
+	
+	public int getCanopy() {
+		return canopy;
+	}
 }
