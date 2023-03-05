71865212bc78d0c69c86d595c8525b5bd3992f9a
awoowen <anguyen307@horizon.csueastbay.edu>
2023-03-05 11:11:32 -0800

    Testing commit from IntelliJ

--- G9_Earth_Base/src/crm/Faunaboard.java
+++ G9_Earth_Base/src/crm/Faunaboard.java
@@ -2,9 +2,32 @@ package crm;
 
 public class Faunaboard {
 
-	
-	public Faunaboard() {
-		// TODO Auto-generated constructor stub
+	private String fauna1;
+	private String fauna2;
+	private String fauna3;
+	private String fauna4;
+	private String eco1;
+	private String eco2;
+	private int First = 15,
+			Second= 11,
+			Third = 8,
+			Fourth = 6,
+			Fifth = 5;
+
+	public Faunaboard(String FAUNA1, String FAUNA2, String FAUNA3, String FAUNA4,
+					  String sharedECO1, String sharedECO2)
+	{
+		fauna1 = FAUNA1;
+		fauna2 = FAUNA2;
+		fauna3 = FAUNA3;
+		fauna4 = FAUNA4;
+		eco1 = sharedECO1;
+		eco2 = sharedECO2;
+
 	}
 
+	public void Points() {
+//        If a player meets requirements for a Fauna, set them in 1st, 2nd, 3rd, 4th, or 5th
+//        to achieve victory points.
+	}
 }
--- G9_Earth_Base/src/crm/Player.java
+++ G9_Earth_Base/src/crm/Player.java
@@ -1,11 +1,12 @@
 package crm;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Player {
 
 	private Card[][] island_board = new Card[4][4];
-	private ArrayList<Card> hand;
+	protected ArrayList<Card> hand;
 	private int soilToken;
 	private int leafToken;
 	private Card ecosystemCard;
@@ -14,11 +15,84 @@ public class Player {
 	private boolean firstPlayer;
 	private boolean activePlayer;
 	private int victoryPoints;
-	
-	public Player() {
-		island_board = null;
+
+	private int chooseCard;
+	private final static Scanner stdin = new Scanner(System.in);
+
+	public Player(CardAmericanSycamore card) {
+//		island_board = null;
 		hand = new ArrayList<Card>();
-		
+
+		DrawCards();
+
 	}
 
+	public void DrawCards() {
+		for(int i = 0; i < 16; i++) {
+			hand.add(EarthDeck.dealTopEarthCard());
+		}
+	}
+
+	public void DisplayHand() {
+		int x = 0;
+		for (Card handCards : hand) {
+			x += 1;
+			System.out.println(x + ": " + handCards.getName());
+		}
+	}
+
+	public Object playCard() {
+
+		chooseCard = -1;
+
+		if (hand.size() > 0){
+			System.out.println("Choose a card to play from your hand");
+			System.out.println("------------------------------------");
+			DisplayHand();
+			System.out.println("------------------------------------");
+			System.out.print("Select Card (1-" + hand.size() + "): ");
+			chooseCard = stdin.nextInt() - 1;
+			stdin.nextLine();
+//			System.out.println("chooseCard: " + chooseCard);
+
+			placeCardOnTableau();
+		}
+		else{
+			System.out.println("No cards in hand.");
+		}
+		return null;
+	}
+
+	public void placeCardOnTableau() {
+
+		int row = -1;
+		int col = -1;
+
+		System.out.println("Choose the row and column of the Tableau to place your card");
+
+		System.out.print("Row: ");
+		row = stdin.nextInt() - 1;
+
+		System.out.print("Column: ");
+		col = stdin.nextInt() - 1;
+
+		stdin.nextLine();
+//		System.out.println("Row = " + row + "\nColumn = " + col);
+
+		island_board[row][col] = hand.remove(chooseCard);
+
+		showTableau();
+	}
+
+	public void showTableau() {
+//		System.out.println(Arrays.deepToString(island_board));
+
+		System.out.println("=Tableau=");
+		for (int a = 0; a < 4; a++) {
+			for (int b = 0; b < 4; b++) {
+				System.out.print(island_board[a][b] + " | ");
+			}
+			System.out.println("\n---------------------------");
+		}
+	}
 }
