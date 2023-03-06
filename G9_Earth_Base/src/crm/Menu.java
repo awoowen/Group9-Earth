f0cd48972423088687faec3901d6904417f27295
Falenheart <Austinly0@aim.com>
2023-03-05 18:49:37 -0800

    Names to GitHub usernames

--- G9_Earth_Base/src/crm/Menu.java
+++ G9_Earth_Base/src/crm/Menu.java
@@ -9,6 +9,26 @@ public class Menu {
 
     public static void beginScreen()
     {
+        //Match names to GitHub names and add group number
+        //Make title screen
+
+        //Even though we were told to limit the amount of System.out.println(), the code just wouldn't look nice if I did it another way
+        System.out.println("=======================================================================");
+        System.out.println("Earth game prototype.");
+        System.out.println("Andrew Nguyen : awoowen");
+        System.out.println("Austin Ly     : Falenheart");
+        System.out.println("Derek Trinh   : tcderk");
+        System.out.println("Joseph Kim    : JoeyAlphaBBQ");
+        System.out.println("=======================================================================");
+
+
+
+
+
+
+
+
+
         do {
             System.out.println("How many players are playing?");
             //Only 1-5 people can play
