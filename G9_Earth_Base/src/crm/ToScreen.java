830ce49dba71c8f9edf85650890ff1c065ae1d85
Falenheart <Austinly0@aim.com>
2023-03-05 17:57:23 -0800

    Worked a bit on ToScreen. Will see if it works out

--- G9_Earth_Base/src/crm/ToScreen.java
+++ G9_Earth_Base/src/crm/ToScreen.java
@@ -1,5 +1,8 @@
 package crm;
 //Most likely, I will be changing the class name, but I pretty much want something first
-public class ToScreen {
-
+public class ToScreen<T> {
+    public static <T>void toScreen(T obj)
+    {
+        System.out.println(obj.toString());
+    }
 }
